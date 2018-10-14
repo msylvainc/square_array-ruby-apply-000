ef3e263a6a2db2556ddb2d0a0efcb499a3fa17dc
Michael Cerasi <michaelcerasi@hotmail.com>
2018-10-14 17:28:28 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,12 @@
 def square_array(array)
-  # your code here
+      array.each ([1,2,3])
+      square_array(numbers)
+# => [1,4,9])
+
+def square_array(array)
+      array.each ([9,10,16,25])
+
+      square_array(new_numbers)
+# => [81,100,256,625]
+    
 end
