b6f4db178c798b7b221efcb9e257d69da5744af3
Michael Cerasi <michaelcerasi@hotmail.com>
2018-10-14 17:28:08 +0000

    automatically backed up by learn

--- square_array.rb
+++ square_array.rb
@@ -1,7 +1,12 @@
 def square_array(array)
-      array.each ([1,2,3])    
+      array.each ([1,2,3])
       square_array(numbers)
 # => [1,4,9])
 
-      end
+def square_array(array)
+      array.each ([9,10,16,25])
+
+      square_array(new_numbers)
+# => [81,100,256,625]
+    
 end
